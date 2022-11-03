f3d3476a2b36962669d6038b6b64bc2aa2432639
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-11-03 21:56:39 +0100

    Circuit breaker

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -1,13 +1,9 @@
 package com.thomasvitale.edgeservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +18,8 @@ public class EdgeServiceApplication {
     }
 
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(exchange -> exchange.matchers(EndpointRequest.toAnyEndpoint()).permitAll()
-                        .anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .build();
+    KeyResolver keyResolver() {
+        return exchange -> Mono.just("ANONYMOUS");
     }
 
 }
