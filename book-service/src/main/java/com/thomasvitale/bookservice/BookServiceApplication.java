bf4bf08a8459fae5014951de908c859b9fd80fc5
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-01-16 20:09:14 +0100

    Upgrade Spring, Grafana and Gradle

--- book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
--- book-service/src/test/java/com/thomasvitale/bookservice/BookServiceApplicationTests.java
+++ book-service/src/test/java/com/thomasvitale/bookservice/BookServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.thomasvitale.bookservice;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
