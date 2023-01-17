d9bd96846a2142fd2dcb768a209938c75c817479
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-01-17 07:42:35 +0100

    Declarative client sample

--- book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
@@ -7,8 +7,13 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class BookServiceApplication {
@@ -16,6 +21,13 @@ public class BookServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
     }
+
+	@Bean
+	DemoService demoService(WebClient.Builder builder) {
+		WebClient client = builder.baseUrl("https://httpbin.org/").build();
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+		return factory.createClient(DemoService.class);
+	}
 
 }
 
@@ -26,7 +38,13 @@ class BookController {
 
     private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
-    @GetMapping("books")
+	private final DemoService demoService;
+
+	BookController(DemoService demoService) {
+		this.demoService = demoService;
+	}
+
+	@GetMapping("books")
     public Iterable<Book> getBooks() {
         log.info("Returning list of books in the catalog");
         return List.of(
@@ -35,4 +53,14 @@ class BookController {
                 new Book("The Lord of the Rings"));
     }
 
+	@GetMapping("/")
+	public String demo() {
+		return demoService.get();
+	}
+
+}
+
+interface DemoService {
+    @GetExchange("/")
+	String get();
 }
